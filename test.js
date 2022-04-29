e610517339cf319ae683a5c9abfefaf317060aeb
Cetin CAKIR <61231974+cetinca@users.noreply.github.com>
2022-04-29 14:43:18 +0300

    Daily commit

--- test.js
+++
@@ -1,4 +0,0 @@
-let textData = "I have got a date at a quarter to eight; I'll see you at the gate, so don’t be late";
-let position = textData.search(/['']/);
-
-console.log(position);
--- test.sh
+++
@@ -1,6 +0,0 @@
-# to read a line
-file_name="definitions.txt"
-line_number=2
-line=$(sed "${line_number}!d" "$file_name")
-read -a text <<< "$line"
-echo ${text[*]}
