2a3bd040a4c4442cf2a8838e9e59542bd467edf8
Cetin CAKIR <61231974+cetinca@users.noreply.github.com>
2022-05-02 17:31:45 +0300

    Daily commit

--- datastructures_algorithms/test.py
+++ datastructures_algorithms/test.py
@@ -1,1 +1,13 @@
-print("car")
+def fibonacci(num):
+    if num == 0 or num == 1:
+        return num
+    return fibonacci(num - 1) + fibonacci(num - 2)
+
+def seq(target):
+    x = 0
+    while x <= target:
+        yield fibonacci(x)
+        x += 1
+
+for i in seq(20):
+    print(i)
