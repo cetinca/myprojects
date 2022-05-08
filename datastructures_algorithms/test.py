96894ef55f363c39bbc8c04f3ac5341affddd5ef
Cetin CAKIR <cetincakirtr@gmail.com>
2022-05-08 18:32:38 +0300

    Daily commit

--- datastructures_algorithms/test.py
+++ datastructures_algorithms/test.py
@@ -1,13 +1,6 @@
-def fibonacci(num):
-    if num == 0 or num == 1:
-        return num
-    return fibonacci(num - 1) + fibonacci(num - 2)
+def mul(a, b):
+    return a * b
 
-def seq(target):
-    x = 0
-    while x <= target:
-        yield fibonacci(x)
-        x += 1
+result = mul(b=3, a=3)
 
-for i in seq(20):
-    print(i)
+print(result)
