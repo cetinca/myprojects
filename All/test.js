1ee922088e82e1ec6fb2d8b94782ab42cfff3e79
Cetin CAKIR <cetincakirtr@gmail.com>
2022-05-11 06:47:16 +0300

    Daily commit

---
+++ All/test.js
@@ -0,0 +1,6 @@
+console.log("Hello! My name is Aid.");
+console.log("I was created in 2020.");
+console.log("Please, remind me your name.");
+// use input() + to get the user's output
+let your_name = "Scully"
+console.log(`What a great name you have, ${your_name}!`);
