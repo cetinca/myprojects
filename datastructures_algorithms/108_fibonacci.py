2d28ff7c23fcbe484dfe7654a24e5acfd4a086ed
Cetin CAKIR <cetincakirtr@gmail.com>
2022-05-15 16:33:47 +0300

    Daily commit

--- datastructures_algorithms/108_fibonacci.py
+++ datastructures_algorithms/108_fibonacci.py
@@ -1,15 +1,32 @@
-def fibo(n):
-    if n < 2:
-        return n
-    return fibo(n-1) + fibo(n-2)
+import re
 
-def fibo_lst():
-    num = 0
+
+def fibonacci(num):
+    if num < 2:
+        return num
+    return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+def get_input():
     while True:
-        yield fibo(num)
-        num += 1
+        user_input = input("Type an integer number below 29: ")
+        pattern = r"^[12]?[0-9]$"
+        if re.match(pattern, user_input):
+            return int(user_input)
 
-gen = fibo_lst()
 
-for _ in range(20):
-    print(next(gen))
+def fibo_gen(_num):
+    num = _num
+    j = 0
+    while j <= num:
+        yield fibonacci(j)
+        j += 1
+
+
+nums = fibo_gen(get_input())
+while True:
+    try:
+        print(next(nums))
+    except StopIteration as e:
+        print(e)
+        break
