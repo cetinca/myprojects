d9f05a0affc0d363b5e20b6bc0dff55706fa8bfb
Cetin CAKIR <cetincakirtr@gmail.com>
2022-10-21 12:12:12 +0300

    Daily commit

--- algos/sort_numbers_or_strings.py
+++ algos/sort_numbers_or_strings.py
@@ -1,8 +1,13 @@
 w = ["asd", "axy", "b", "e", "a", "A", "AB", "Ab"]
 n = [5, 4, 99, 102, 17, -5]
+m = [4, 4, "car"]
 
 
 def invert(lst):
+    for item in lst:
+        if item.__class__() != lst[0].__class__():
+            raise TypeError("List is multi-type, can not be sorted!")
+
     if type(lst[0]) is str:
         return [word.swapcase() for word in lst]
     else:
@@ -10,15 +15,20 @@ def invert(lst):
 
 
 def sort_lst(lst):
-    lst = invert(lst)
-    for _ in range(len(lst) - 1):
-        for i in range(len(lst) - 1):
-            if lst[i] > lst[i + 1]:
-                lst[i], lst[i + 1] = lst[i + 1], lst[i]
-    return invert(lst)
+    try:
+        lst = invert(lst)
+        for _ in range(len(lst) - 1):
+            for i in range(len(lst) - 1):
+                if lst[i] > lst[i + 1]:
+                    lst[i], lst[i + 1] = lst[i + 1], lst[i]
+        return invert(lst)
+    except TypeError as err:
+        return err
 
 
 r = sort_lst(w)
 print(r)
 r = sort_lst(n)
+print(r)
+r = sort_lst(m)
 print(r)
